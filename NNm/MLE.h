2f09e9a01d39523f85169ca017e8299202ec0e6a
Douglas Santry <deplume@gmail.com>
2022-07-14 19:35:36 +0100

    Missed these new files.

---
+++ NNm/MLE.h
@@ -0,0 +1,164 @@
+/*
+
+Copyright (c) 2020, Douglas Santry
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, is permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef _NN_SOFTMAX_MLE__H__
+#define _NN_SOFTMAX_MLE__H__
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+
+#include <NNm.h>
+
+/*
+ * Implements a layer when training a neural network.
+ *
+ */
+struct SoftmaxMLE_t : public stratum_t
+{
+	// Matrices - per weight, s_Nnodes x s_Nin
+	NeuralM_t				ml_W;
+	NeuralM_t				ml_dL;
+
+	Softmax_t				ml_softm;
+
+	IEEE_t					ml_guess;
+
+	SoftmaxMLE_t (const int ID,
+			const int K, 
+			const int Nin,
+			StrategyAlloc_t rule) :
+		stratum_t ("softmax", ID, K, Nin + 1),	// account for bias
+		ml_W (K, Nin + 1),
+		ml_dL (K, Nin + 1),
+		ml_softm (K),
+		ml_guess (-1)
+	{
+		s_strat = (*rule) (K, Nin + 1, ml_W.raw (), ml_dL.raw ());
+	}
+
+	virtual ~SoftmaxMLE_t (void)
+	{
+	}
+
+	void _sAPI_init (void)
+	{
+		int Nout = ml_softm.K ();
+
+		// Glorot, W ~ [-r, r]
+		IEEE_t r = sqrt (6.0 / (Nout + s_Nin));
+		IEEE_t *p = ml_W.raw();
+		IEEE_t sample;
+
+		for (int i = ml_W.rows () - 1; i >= 0; --i)
+			for (int j = ml_W.columns () - 1; j >= 0; --j)
+			{
+				sample = (IEEE_t) rand () / RAND_MAX;
+				sample *= r;
+				if (rand () % 2)
+					sample = -sample;
+				*p++ = sample;
+			}
+
+		ml_dL.zero ();
+	}
+
+	virtual IEEE_t * _sAPI_f (IEEE_t * const, bool = true);
+	virtual void _sAPI_gradient (stratum_t &);
+	virtual void _sAPI_bprop (IEEE_t *, bool = true);
+	virtual IEEE_t _sAPI_Loss (IEEE_t const * const);
+
+	virtual void StrategyMono (const int index)
+	{
+		return; // over-ride when debugging or instrumenting
+	}
+};
+
+void
+SoftmaxMLE_t::_sAPI_gradient (stratum_t &Z)
+{
+	Z.s_delta.TransposeMatrixVectorMult (ml_W, s_delta.raw ());
+}
+
+void 
+SoftmaxMLE_t::_sAPI_bprop (IEEE_t *xi, bool activation)
+{
+	// Apply the delta for per weight derivatives
+
+	IEEE_t *dL = ml_dL.sm_data;
+	IEEE_t delta;
+
+	/*
+	 * ∂L       ∂∑
+	 * -- = 𝛿 · -- = 𝛿 · Xi
+	 * ∂w       ∂w
+	 *
+	 * ∆W = 𝛿 · transpose (Xi), this is an outer product, but we do 
+	 * it here instead of NeuralM.
+	 *
+	 */
+	for (int i = 0; i < s_Nnodes; ++i)
+	{
+		delta = s_delta.sm_data[i];
+		*dL++ += delta; // the Bias
+
+assert (!isnan (delta) && !isinf (delta));
+
+		for (int j = 1; j < s_Nin; ++j)
+{
+assert (!isnan (*dL) && !isinf (*dL));
+			*dL++ += delta * xi[j - 1];
+}
+	}
+}
+
+IEEE_t *
+SoftmaxMLE_t::_sAPI_f (IEEE_t * const xi, bool activate)
+{
+	s_response.MatrixVectorMult (ml_W, xi);
+
+	ml_guess = ml_softm.ComputeSoftmax (s_response.raw ());
+
+	return &ml_guess;
+}
+
+IEEE_t
+SoftmaxMLE_t::_sAPI_Loss (IEEE_t const * const answers)
+{
+	IEEE_t loss = -log (ml_softm.P (answers[0]));
+	if (isnan (loss) || isinf (loss))
+		loss = -1;
+
+	ml_softm.bprop (answers[0], s_delta.raw ());
+
+	return loss;
+}
+
+#endif // header inclusion
+
---
+++ NNm/MSE.h
@@ -0,0 +1,126 @@
+/*
+
+Copyright (c) 2022, Douglas Santry
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, is permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef _NN_MSE_REGRESSION__H__
+#define _NN_MSE_REGRESSION__H__
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+
+#include <NNm.h>
+
+/*
+ * Implements a layer when training a neural network.
+ *
+ */
+struct ScalerMSE_t : public stratum_t
+{
+	NeuralM_t				ms_W;
+	NeuralM_t				ms_dL;
+
+	ScalerMSE_t (const int ID, const int Nin, StrategyAlloc_t rule) :
+		stratum_t ("MSE", ID, 1, Nin),
+		ms_W (1, Nin + 1),
+		ms_dL (1, Nin + 1)
+	{
+		s_strat = (*rule) (1, Nin + 1, ms_W.raw (), ms_dL.raw ());
+	}
+
+	virtual ~ScalerMSE_t (void)
+	{
+	}
+
+	void _sAPI_init (void)
+	{
+		for (int i = 0; i < s_Nin; ++i)
+			ms_W.sm_data[i] = rand () / RAND_MAX - 1.0;
+
+		ms_dL.zero ();
+
+	}
+
+	virtual IEEE_t * _sAPI_f (IEEE_t * const, bool = true);
+	virtual void _sAPI_gradient (stratum_t &);
+	virtual void _sAPI_bprop (IEEE_t *, bool = true);
+	virtual IEEE_t _sAPI_Loss (IEEE_t const * const);
+
+	virtual void StrategyMono (const int index)
+	{
+		return; // over-ride when debugging or instrumenting
+	}
+};
+
+void
+ScalerMSE_t::_sAPI_gradient (stratum_t &Z)
+{
+	Z.s_delta.TransposeMatrixVectorMult (ms_W, s_delta.raw ());
+}
+
+void 
+ScalerMSE_t::_sAPI_bprop (IEEE_t *xi, bool activation)
+{
+	s_delta.sm_data[0] *= DERIVATIVE_FN (s_response.sm_data[0]);
+
+	IEEE_t *dL = ms_dL.sm_data;
+	IEEE_t delta = s_delta.sm_data[0];
+
+	*dL++ += delta; // the Bias
+
+	for (int i = 1; i < s_Nin; ++i)
+		*dL++ += delta * xi[i - 1];
+}
+
+IEEE_t *
+ScalerMSE_t::_sAPI_f (IEEE_t * const xi, bool activate)
+{
+	s_response.MatrixVectorMult (ms_W, xi);
+
+	IEEE_t *f = s_response.raw ();
+
+	if (activate)
+		for (int i = 0; i < s_Nin; ++i, ++f)
+			*f = ACTIVATION_FN (*f);
+
+	return s_response.raw ();
+}
+
+IEEE_t
+ScalerMSE_t::_sAPI_Loss (IEEE_t const * const answers)
+{
+	IEEE_t loss = 0.0;
+
+	loss = s_delta.sm_data[0] = s_response.sm_data[0] - answers[0];
+	loss *= loss;
+
+	return 0.5 * loss;
+}
+
+#endif // header inclusion
+
