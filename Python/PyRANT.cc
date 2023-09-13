544c4c68e61f8889851cc6377d100c4b4dac0501
Douglas Santry <deplume@gmail.com>
2023-09-13 15:17:08 +0100

    Python bindings.

---
+++ Python/PyRANT.cc
@@ -0,0 +1,265 @@
+/*
+
+Copyright (c) 2022, Douglas Santry
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, is permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include <sys/param.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+#include <NNm.h>
+#include <options.h>
+#include <read_csv.h>
+#include <MNIST.h>
+
+#define N_POINTS	32
+
+#define PI          3.141592653589793
+#define PI_2        1.570796326794897
+
+#define RANGE       PI_2
+
+extern "C"
+{
+
+void SeedRandom (long seed)
+{
+	srand (seed);
+}
+
+NNet_t * AllocNNm (int Nhidden, int Nin, int Nout)
+{
+	NNet_t *Np = NULL;
+
+	Np = new NNet_t (Nhidden, Nin, Nout);
+
+	Np->SetKeepAlive (0);
+
+	return Np;
+}
+
+void SetKeepAlive (NNet_t *Np, int modulus)
+{
+	Np->SetKeepAlive (modulus);
+}
+
+void SetStopLoss (NNet_t *Np, double halt)
+{
+	Np->SetHalt (halt);
+}
+
+void SetMaxIterations (NNet_t *Np, int N)
+{
+	Np->SetMaxIterations (N);
+}
+
+void SetUseSGD (NNet_t *Np, double percentBatchSize)
+{
+	Np->SetSGD (percentBatchSize);
+}
+
+void AddDense (NNet_t *Np, int N, bool useADAM)
+{
+	Np->AddDenseLayer (N, (useADAM ? ADAM : RPROP));
+}
+
+void AddMSE (NNet_t *Np, bool useADAM)
+{
+	Np->AddScalerMSELayer (useADAM ? ADAM : RPROP);
+}
+
+void AddFilter (NNet_t *Np, int N, int width, int stride, bool useADAM)
+{
+	Np->Add2DFilterLayer (N, width, stride, useADAM ? ADAM : RPROP);
+}
+
+void AddMaxPool (NNet_t *Np, int N, int width, int stride)
+{
+	Np->Add2DMaxPoolLayer (N, width, stride);
+}
+
+void AddSoftmax (NNet_t *Np, bool useADAM)
+{
+	Np->AddSoftmaxLayer (useADAM ? ADAM : RPROP);
+}
+
+void AddMultiCLayer (NNet_t *Np, bool useADAM)
+{
+	Np->AddMultiCLayer (useADAM ? ADAM : RPROP);
+}
+
+void Shape (NNet_t *Np)
+{
+	Np->DisplayShape ();
+}
+
+void Arch (NNet_t *Np)
+{
+	Np->DisplayModel ();
+}
+
+void Train (NNet_t *Np, DataSet_t *O, int steps)
+{
+	try {
+
+		Np->Train (O, steps);
+
+	} catch (const char *msgp) {
+
+		printf ("%s\n", msgp);
+	}
+}
+
+double Inference (NNet_t *Np, double *predictors)
+{
+	return Np->Compute (predictors);
+}
+
+int Classify (NNet_t *Np, double *predictors)
+{
+	return (int) Np->Compute (predictors);
+}
+
+double * ClassifyVec (NNet_t *Np, double *predictors)
+{
+	double *y = Np->ComputeVec (predictors);
+
+	return y;
+}
+
+void SetPreProcessing (NNet_t *Np, DataSet_t *O)
+{
+	Np->AddPreProcessingLayer (O);
+}
+
+double Loss (NNet_t *Np)
+{
+	return Np->Loss ();
+}
+
+int Steps (NNet_t *Np)
+{
+	return Np->Steps ();
+}
+
+DataSet_t *LoadMNIST (char const * const path)
+{
+	char fullpath_data [MAXPATHLEN];
+	char fullpath_labels [MAXPATHLEN];
+
+	snprintf (fullpath_data, MAXPATHLEN, "%s/train-images.idx3-ubyte", path);
+	snprintf (fullpath_labels, MAXPATHLEN, "%s/train-labels.idx1-ubyte", path);
+    MNIST_t data (fullpath_data, fullpath_labels);
+
+	data.Steal ();
+
+	return data.Data ();
+}
+
+DataSet_t *LoadCSVClass (char const * const fileName, 
+	const int N, 
+	int *accept)
+{
+	LoadCSV_t Z (fileName);
+	bool *columns = new bool [N];
+
+	for (int i = 0; i < N; ++i)
+		columns[i] = accept[i] ? true : false;
+
+	DataSet_t *O = Z.LoadDS (N, columns);
+
+	delete [] columns;
+
+	return O;
+}
+
+
+DataSet_t *LoadCSV (char const * const fileName, 
+	const int N, 
+	const int Nin,
+	const int Nout,
+	int *accept)
+{
+	LoadCSV_t Z (fileName);
+	bool *columns = new bool [N];
+
+	for (int i = 0; i < N; ++i)
+		columns[i] = accept[i] ? true : false;
+
+	DataSet_t *O = Z.LoadDS (N, columns);
+	O->Split (Nin, Nout); // let data know length of answer
+
+	delete [] columns;
+
+	return O;
+}
+
+void DisplayData (DataSet_t const * const O)
+{
+	O->Display ();
+}
+
+DataSet_t *BuildTrainingSet (int N)
+{
+	DataSet_t *O = new DataSet_t (N, 1, 1);
+
+	for (int i = 0; i < N; ++i)
+	{
+		double sample = (double) rand () / RAND_MAX;
+
+		(*O)[i][0] = sample;
+		(*O)[i][1] = sin ((*O)[i][0] * RANGE);
+	}
+
+	return O;
+}
+
+double *ExtractTuple (DataSet_t *O, int index)
+{
+	return (*O)[index];
+}
+
+double Answer (DataSet_t *O, int index)
+{
+	return O->Answer (index);
+}
+
+double *AnswerVec (DataSet_t *O, int index)
+{
+	double *p = O->AnswerVec (index);
+
+	return p;
+}
+
+int DataLen (DataSet_t *O)
+{
+	return O->N ();
+}
+
+}
+
---
+++ Python/PyRANTAPI.py
@@ -0,0 +1,156 @@
+import ctypes as ct
+import time as time
+
+NNmL = ct.cdll.LoadLibrary('libRANT.so')
+
+NNmL.AllocNNm.restype = ct.POINTER (ct.c_int)
+
+NNmL.BuildTrainingSet.restype = ct.POINTER (ct.c_int)
+
+NNmL.LoadMNIST.restype = ct.POINTER (ct.c_int)
+NNmL.LoadMNIST.argtypes = [ct.c_char_p]
+
+NNmL.LoadCSVClass.restype = ct.POINTER (ct.c_int)
+NNmL.LoadCSVClass.argtypes = [ct.c_char_p, ct.c_int, ct.POINTER (ct.c_int)]
+
+NNmL.LoadCSV.restype = ct.POINTER (ct.c_int)
+NNmL.LoadCSV.argtypes = [ct.c_char_p, ct.c_int, ct.c_int, ct.c_int, ct.POINTER (ct.c_int)]
+
+NNmL.SetUseSGD.argtypes = [ct.POINTER (ct.c_int), ct.c_double]
+
+NNmL.Inference.restype = ct.c_double
+NNmL.Inference.argtypes = [ct.POINTER (ct.c_int), ct.POINTER (ct.c_int)]
+
+NNmL.Classify.argtypes = [ct.POINTER (ct.c_int), ct.POINTER (ct.c_int)]
+
+NNmL.ClassifyVec.argtypes = [ct.POINTER (ct.c_int), ct.POINTER (ct.c_int)]
+NNmL.ClassifyVec.restype = ct.POINTER (ct.c_double)
+
+NNmL.SetStopLoss.argtypes = [ct.POINTER (ct.c_int), ct.c_double]
+
+NNmL.Loss.restype = ct.c_double
+
+NNmL.ExtractTuple.restype = ct.POINTER (ct.c_int)
+NNmL.ExtractTuple.argtypes = [ct.POINTER (ct.c_int), ct.c_int]
+
+NNmL.Answer.restype = ct.c_double
+NNmL.AnswerVec.restype = ct.POINTER (ct.c_double)
+
+class NNm:
+
+	def __init__ (self, hidden, Nin, Nout, seed = None):
+
+		self.Nin = Nin
+		self.Nout = Nout
+
+		if (seed is None):
+			seed = int (time.time ())
+
+		NNmL.SeedRandom (seed)
+		self.Model = NNmL.AllocNNm (hidden, Nin, Nout)
+
+	def SetKeepAlive (self, N):
+		NNmL.SetKeepAlive (self.Model, N)
+
+	def SetStopLoss (self, haltVal):
+		NNmL.SetStopLoss (self.Model, haltVal)
+
+	def SetSGD (self, percentBatchSize):
+		NNmL.SetUseSGD (self.Model, percentBatchSize)
+
+	def SetMaxIterations (self, N):
+		NNmL.SetUseSGD (self.Model, N)
+
+	def AddPreProcessing (self, dataset):
+		NNmL.SetPreProcessing (self.Model, dataset)
+
+	def AddDense (self, N, ADAM = True):
+		NNmL.AddDense (self.Model, N, ADAM)
+
+	def AddConvFilter (self, N, width, stride, ADAM = True):
+		NNmL.AddFilter (self.Model, N, width, stride, ADAM)
+
+	def AddConvMaxPool (self, N, width, stride):
+		NNmL.AddMaxPool (self.Model, N, width, stride)
+
+	def AddMSE (self, ADAM = True):
+		NNmL.AddMSE (self.Model, ADAM)
+
+	def AddSoftmax (self, ADAM = True):
+		NNmL.AddSoftmax (self.Model, ADAM)
+
+	def AddMultiCLayer (self, ADAM = True):
+		NNmL.AddMultiCLayer (self.Model, ADAM)
+
+	def TestSet (self):
+		return NNmL.BuildTrainingSet (32)
+
+	def Train (self, TrainingSet, steps):
+		NNmL.Train (self.Model, TrainingSet, steps)
+
+	def Inference (self, predictors):
+		return NNmL.Inference (self.Model, predictors)
+
+	def Classify (self, predictors):
+		return NNmL.Classify (self.Model, predictors)
+
+	def ClassifyVec (self, predictors):
+		__y = NNmL.ClassifyVec (self.Model, predictors)
+		y = []
+		for i in range (self.Nout):
+			y.append (__y[i])
+
+		return y
+
+	def Arch (self):
+		NNmL.Arch (self.Model)
+
+	def Shape (self):
+		NNmL.Shape (self.Model)
+
+	def Loss (self):
+		return NNmL.Loss (self.Model)
+
+	def Steps (self):
+		return NNmL.Steps (self.Model)
+
+	def LoadMNIST (self, path):
+		c_str = path.encode ("utf-8")
+		return NNmL.LoadMNIST (c_str)
+
+	def LoadFileClass (self, fileName, accept):
+		Clist = (ct.c_int * len (accept)) (*accept)
+		columns = len (accept)
+		c_str = fileName.encode ("utf-8")
+		return NNmL.LoadCSVClass (c_str, columns, Clist);
+
+	def LoadFile (self, fileName, Nin, Nout, accept):
+		Clist = (ct.c_int * len (accept)) (*accept)
+		c_str = fileName.encode ("utf-8")
+		return NNmL.LoadCSV (c_str, Nin + Nout, Nin, Nout, Clist);
+
+	def DisplayData (self, O):
+		return NNmL.DisplayData (O)
+
+	def ExtractTuple (self, O, index):
+		return NNmL.ExtractTuple (O, index)
+
+	def Answer (self, O, index):
+		y = NNmL.Answer (O, index)
+#		y_list = []
+#		y_list.append (y[0])
+
+#		return y_list
+		return y
+
+	def AnswerVec (self, O, index):
+		__y = NNmL.AnswerVec (O, index)
+		y = []
+		for i in range (self.Nout):
+			y.append (__y[i])
+
+		return y
+
+	def DataLen (self, O):
+		return NNmL.DataLen (O)
+
